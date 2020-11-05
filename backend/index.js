c5bc17982e3e66cf8f91624cae95bd34141143d9
Carlos <carlos.arau.lopez@gmail.com>
2020-11-05 23:23:36 +0100

    jwt creation in the backend

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,54 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3000
+const port = 3000;
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
+app.set('key','secretkey')
 app.use(cors())
 app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+app.post('/login', (req, res) => {
+  if(req.body.user === "carlos" && req.body.password === "helloworld") {
+  const payload = {
+    check:  true
+  };
+  const token = jwt.sign(payload, app.get('key'), {
+    expiresIn: 1440
+  });
+  res.json({
+    mensaje: 'Authentication ok',
+    token: token
+  });
+  } else {
+      res.json({ mensaje: "Incorrect User or Password"})
+  }
+})
+
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+ 
+    if (token) {
+      jwt.verify(token, app.get('key'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Invalid Token' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token missing' 
+      });
+    }
+ });
 
 //ccreativo table
 app.get('/offers', db.getOffers)
